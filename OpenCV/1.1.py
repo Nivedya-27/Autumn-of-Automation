36949540bec725dc16e516bef311cf73266f9ee2
Nivedya-27 <190070039@iitb.ac.in>
2020-07-24 21:54:39 +0530

    edited qn1 code

--- OpenCV/1.1.py
+++ OpenCV/1.1.py
@@ -1,15 +1,17 @@
 import cv2 as cv
 import numpy as np
 import sys
+import os
 img=cv.imread('test.png')
 if img is None:
 	sys.exit('could not read the image; check directory')
 
 grey=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 ret,bw=cv.threshold(grey,127,255,cv.THRESH_BINARY)
-images=[img,grey,bw]
-titles=['original','grayscale','black and white']
-for i in range(3):
-	cv.imshow(titles[i],images[i])
-cv.waitKey(0)
-cv.destroyAllWindows()
+images=[grey,bw]
+titles=['grayscale','black and white']
+os.makedirs('bw_gray')
+for i in range(2):
+		cv.imwrite(os.path.join('bw_gray/',(titles[i]+'.png')),images[i])
+if cv.waitKey(0)==27:
+	cv.destroyAllWindows()
--- OpenCV/1.3.py
+++ OpenCV/1.3.py
@@ -1,10 +1,13 @@
 import cv2 as cv
 import numpy as np
 import sys
-img=cv.imread('test.png')
-if img is None:
+bgr=cv.imread('test.png')
+if bgr is None:
 	sys.exit('could not open image')
-new=cv.cvtColor(img,cv.COLOR_BGR2RGB)
-cv.imshow('converted',new)
-cv.waitKey(0)
-cv.destroyAllWindows()
+rgb=cv.cvtColor(bgr,cv.COLOR_BGR2RGB)
+img=bgr.copy()
+img[:,:,2]=0
+img[:,:,0]=img[:,:,0]+rgb[:,:,0]
+cv.imwrite('converted.png',img)
+if cv.waitKey(0)==27:
+	cv.destroyAllWindows()
