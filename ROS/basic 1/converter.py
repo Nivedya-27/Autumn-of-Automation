50137a8e960b37bbc7f612cfd449da1d2d037ba0
Nivedya-27 <190070039@iitb.ac.in>
2020-07-31 10:22:41 +0530

    basic assignment uploaded

--- ROS/basic 1/converter.py
+++ ROS/basic 1/converter.py
@@ -1,26 +1,28 @@
+#!/usr/bin/env python
 import rospy
 import math
-from quaternion.msg import quaternion
-from rpy.msg import eulerangles
+from beginner_tutorials.msg import quaternion
+from beginner_tutorials.msg import eulerangles
 class my_converter(object):
 	def __init__(self):
-		self.angles=(0.0,0.0,0.0)
-		#self.value=(self.yaw,self.pitch,self.roll)
+		self.angles=eulerangles()
+		self.angles.yaw=0
+		self.angles.pitch=0
+		self.angles.roll=0
 		rospy.init_node('my_converter',anonymous=True)
 		rospy.Subscriber('topic1',quaternion,self.convert)
 		self.pub=rospy.Publisher('topic2',eulerangles)
 
 	def convert(self,msg):
-		(x,y,z,w)=msg.data
-		self.roll=math.atan2(2*y*w-2*x*z,1-2*y*y-2*z*z)
-		self.pitch=math.atan2(2*x*w-2*y*z,1-2*x*x-2*z*z)
-		self.yaw=math.asin(2*x*y+2*z*w)
-		self.angles=(self.yaw,self.pitch,self.roll)
+		(x,y,z,w)=(msg.x,msg.y,msg.z,msg.w)
+		self.angles.roll=math.atan2(2*y*w-2*x*z,1-2*y*y-2*z*z)
+		self.angles.pitch=math.atan2(2*x*w-2*y*z,1-2*x*x-2*z*z)
+		self.angles.yaw=math.asin(2*x*y+2*z*w)
 	
 	def run(self):
 		r=rospy.Rate(10)
 		while not rospy.is_shutdown():
-			rospy.loginfo(angles)
+			rospy.loginfo(self.angles)
 			self.pub.publish(self.angles)
 			r.sleep()
 
